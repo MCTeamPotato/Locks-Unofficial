3382bdd01794d1ebd4ae11e094ba45796af4c078
falsidge <tesetings@gmail.com>
2025-01-11 11:15:13 -0800

    Fix server crashing in network code. See https://fabricmc.net/2024/04/19/1205.html "you need to register it in ModInitializer on both ends"

--- src/main/java/melonslise/locks/common/network/toclient/ClientNet.java
+++ src/main/java/melonslise/locks/common/network/toclient/ClientNet.java
@@ -6,11 +6,7 @@ import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 public class ClientNet {
 
     public static void register() {
-        PayloadTypeRegistry.playS2C().register(AddLockablePacket.TYPE, AddLockablePacket.STREAM_CODEC);
-        PayloadTypeRegistry.playS2C().register(AddLockableToChunkPacket.TYPE, AddLockableToChunkPacket.STREAM_CODEC);
-        PayloadTypeRegistry.playS2C().register(RemoveLockablePacket.TYPE, RemoveLockablePacket.STREAM_CODEC);
-        PayloadTypeRegistry.playS2C().register(TryPinResultPacket.TYPE, TryPinResultPacket.STREAM_CODEC);
-        PayloadTypeRegistry.playS2C().register(UpdateLockablePacket.TYPE, UpdateLockablePacket.STREAM_CODEC);
+
         ClientPlayNetworking.registerGlobalReceiver(AddLockablePacket.TYPE, (payload, context) -> {
             context.client().execute(() -> {
                 AddLockablePacket.handle(payload, context.player());
